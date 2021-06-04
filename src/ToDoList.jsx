29fb6fd5e843892deb4a3c3af154347a055ef956
Rajendra <nathrajen2710@gmail.com>
2021-06-05 00:53:02 +0530

    initial commit

---
+++ src/ToDoList.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import ListCom from "./ListCom";
+
+const ToDoList=()=>{
+    const[item,setItem]=useState("");
+    const[newitem,setNewItem]=useState([]);
+    const itemEvent=(event)=>{
+        setItem(event.target.value);
+    };
+    const listOfItems=()=>{
+        setNewItem((prevValue)=>{
+            return[...prevValue,item]
+        });
+        setItem("");
+    }
+    return(
+        <>
+        <div className="main_div">
+            <div className="center_div">
+            <br/>
+            <h1>ToDo List</h1>
+            <br/>
+            <input type="text" value={item} placeholder="Add an Item" onChange={itemEvent}/>
+            <Button className="newBtn" onClick={listOfItems}>
+                <AddIcon className="newBtn"/>
+            </Button>
+            <br/>
+            <ol>
+                
+               { newitem.map((val,index)=>{
+                    return <ListCom key={index} text={val}/>
+                })
+               }
+            </ol>
+            <br/>
+            </div>
+        </div>
+
+        </>
+    )
+}
+
+export default ToDoList;
